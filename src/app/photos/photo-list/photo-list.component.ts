c17729f3a1a572058a9192886b5068ead3d781de
Felipe Dias <adfelipealmeida@gmail.com>
2022-05-06 07:46:58 -0300

    add filter

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-
 import { PhotoService } from '../photo/photo.service';
+import { Photo } from './../photo/photo';
+
 
 @Component({
   selector: 'app-photo-list',
@@ -10,15 +11,16 @@ import { PhotoService } from '../photo/photo.service';
 })
 export class PhotoListComponent implements OnInit {
 
-  photos: any[] = []; 
-  
+  photos: Photo[] = [];
+  filter: String = "";
+
   constructor(
     private photoService: PhotoService,
     private activatedRoute: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
-    
+
       const userName = this.activatedRoute
         .snapshot
         .params
--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { PhotoFormComponent } from './photo-form/photo-form.component';
+import { FilterByDescription } from './photo-list/filter-by-description.pipe';
 import { PhotoListComponent } from './photo-list/photo-list.component';
 import { PhotosComponent } from './photo-list/photos/photos.component';
 import { PhotoComponent } from './photo/photo.component';
@@ -13,6 +14,7 @@ import { PhotoComponent } from './photo/photo.component';
         PhotoListComponent,
         PhotoFormComponent,
         PhotosComponent,
+        FilterByDescription
     ],
     imports: [
         HttpClientModule,
