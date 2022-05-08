f92f81cf38e7a915aa46f29d4df3e739b3411d3a
Felipe Dias <adfelipealmeida@gmail.com>
2022-05-08 18:43:04 -0300

    utilizando resolver

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,19 +1,26 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
-import { PhotoListComponent } from './photos/photo-list/photo-list.component';
-import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
+import { RouterModule, Routes } from '@angular/router';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
+import { PhotoListComponent } from './photos/photo-list/photo-list.component';
+import { PhotoListResolver } from './photos/photo-list/photo-list.resolver';
+
 
 const routes: Routes = [
-    { path: 'user/:userName', component: PhotoListComponent },
+  {
+    path: 'user/:userName',
+    component: PhotoListComponent,
+    resolve: {
+        photos: PhotoListResolver,
+    }
+  },
     { path: 'p/add', component: PhotoFormComponent },
-    { path: '**', component: NotFoundComponent }  
+    { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
-    imports: [ 
-        RouterModule.forRoot(routes) 
+    imports: [
+        RouterModule.forRoot(routes)
     ],
     exports: [ RouterModule ]
 })
