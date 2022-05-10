99b91cd23971d8000aeafccd133047d3a511f185
Felipe Dias <adfelipealmeida@gmail.com>
2022-05-10 08:23:20 -0300

    feito paginação

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -6,6 +6,7 @@ import { FilterByDescription } from './photo-list/filter-by-description.pipe';
 import { PhotoListComponent } from './photo-list/photo-list.component';
 import { PhotosComponent } from './photo-list/photos/photos.component';
 import { PhotoComponent } from './photo/photo.component';
+import { LoadButtonComponent } from './photo-list/load-button/load-button.component';
 
 
 @NgModule({
@@ -14,7 +15,8 @@ import { PhotoComponent } from './photo/photo.component';
         PhotoListComponent,
         PhotoFormComponent,
         PhotosComponent,
-        FilterByDescription
+        FilterByDescription,
+        LoadButtonComponent
     ],
     imports: [
         HttpClientModule,
