dfa0a4d95958a2dcf3347ef7ba243aabda9fa7f5
Felipe Dias <adfelipealmeida@gmail.com>
2022-06-19 19:16:07 -0300

    validação de formulário

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,7 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './signin.component.html'
 })
 
-export class SignInComponent {}
+export class SignInComponent implements OnInit {
+  loginForm: FormGroup; // Controla o form do template
+
+  // Contrutor de formulário
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      // primeiro parâmetro é o valor padrão do formulário.
+      // segundo parâmetro do array é a validação.
+      userName: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+}
