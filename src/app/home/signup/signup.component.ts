cec5d04f5cb332592b8fa5faab17c19525a0fbfb
Felipe Dias <adfelipealmeida@gmail.com>
2022-08-24 08:51:00 -0300

    valida~]ao assincrona

--- src/app/home/signup/signup.component.ts
+++ src/app/home/signup/signup.component.ts
@@ -1,46 +1,48 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { lowerCaseValidator } from 'src/app/shared/components/validators/lower-case.validator';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { lowerCaseValidator } from "src/app/shared/components/validators/lower-case.validator";
+import { UserNotTakenValidatorService } from "./signup-not-taken.validator.service";
 
 @Component({
-    templateUrl: "./signup.component.html"
+  templateUrl: "./signup.component.html",
 })
-export class SignUpComponent implements OnInit{
-    singupForm: FormGroup;
+export class SignUpComponent implements OnInit {
+  singupForm: FormGroup;
 
-    constructor(private formBuilder: FormBuilder) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private userNotTakenValidatorService: UserNotTakenValidatorService
+  ) {}
 
-    ngOnInit(): void {
-        this.singupForm = this.formBuilder.group({
-            email: ['', 
-                [
-                    Validators.required, 
-                    Validators.email
-                ]
-            ],
-            fullName: ['',
-                [
-                    Validators.required,
-                    Validators.minLength(2),
-                    Validators.maxLength(40)
-                ]
-            ],
-            userName: ['',
-                [
-                    Validators.required,
-                    lowerCaseValidator,
-                    Validators.minLength(2),
-                    Validators.maxLength(40)
-                ]
-            ],
-            password: ['',
-                [
-                    Validators.required,
-                    Validators.minLength(8),
-                    Validators.maxLength(14)
-                ]
-            ]
-        })
-    }
-
+  ngOnInit(): void {
+    this.singupForm = this.formBuilder.group({
+      email: ["", [Validators.required, Validators.email]],
+      fullName: [
+        "",
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(40),
+        ],
+      ],
+      userName: [
+        "",
+        [
+          Validators.required,
+          lowerCaseValidator,
+          Validators.minLength(2),
+          Validators.maxLength(40),
+        ],
+        this.userNotTakenValidatorService.checkUserNameTaken(),
+      ],
+      password: [
+        "",
+        [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(14),
+        ],
+      ],
+    });
+  }
 }
